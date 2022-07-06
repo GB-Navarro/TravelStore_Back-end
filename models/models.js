460224cf3f54d66672f2ef180b07485a5d64176e
GB-Navarro <gbnavarro@usp.br>
2022-07-06 16:45:19 -0300

    A bug in the testdb function has been fixed

--- models/models.js
+++ models/models.js
@@ -9,9 +9,10 @@ mongoClient.connect().then(() => {
   db = mongoClient.db("travelStore");
 });
 
-export async function testDB(data){
+export async function testDB(req, res){
+  let data = req.body
   try{
-    let promisse = await db.collections("test").insertOne(data)
+    let promisse = await db.collection("test").insertOne(data)
     console.log("Ok!", await promisse);
   }catch(error){
     console.log("Error!", error)

061fcdd26c1b69d274b4c38c7f94da7b9e65ec8b
GB-Navarro <gbnavarro@usp.br>
2022-07-10 12:24:17 -0300

    Added a trips attribute to the user object

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -3,11 +3,10 @@ import validateUserData from "../functions/validateUserData.js"
 import { createUser, checkEmailExistence, checkUserPassword, createUserSession, checkIfUserHaveAnActiveSession } from "../models/models.js";
 
 export async function signUp(req,res){
-    //criptografar a senha do usuário antes de criar ele (bcrypt)
-    //usar o bcrypt na comparação de senhas
     let registrationData = req.body;
     let isRegistrationDataValid = await validateRegistrationData(registrationData);
     if(isRegistrationDataValid){
+        registrationData.trips = [];
         let isUserCreated = await createUser(registrationData);
         if(isUserCreated){
             res.send("User is created").status(201);
@@ -25,6 +24,7 @@ export async function signIn(req, res){
     let userEmailExists = await checkEmailExistence(userData.email);
     let userPasswordIsValid = await checkUserPassword(userData.password, userData.email);
     let userHaveAnActiveSession = await checkIfUserHaveAnActiveSession(userData.email);
+    //Alterar aqui, primeiro validar o userdata, dps o useremail, dps o userpassword e dps a verificação de sessão ativa (medida de segurança)
     if(isUserDataValid && userEmailExists && userPasswordIsValid && !(userHaveAnActiveSession)){
         let userToken = await createUserSession(userData.email);
         if(userToken != undefined){
